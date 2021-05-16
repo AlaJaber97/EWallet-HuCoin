c6cbf082c26037f72469902c524ea2b2320d2cfc
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-05-16 20:28:58 +0300

    -add some new opration

--- HuCoin/HuCoin/ViewModels/ChangePasswordPageViewModel.cs
+++ HuCoin/HuCoin/ViewModels/ChangePasswordPageViewModel.cs
@@ -13,7 +13,33 @@ namespace HuCoin.ViewModels
         public string ConfirmPassword { get; set; }
         public ChangePasswordPageViewModel()
         {
-
+            SubmitNewPasswordCommand = new Command(() => SubmitNewPassword().ConfigureAwait(false));
+        }
+        private async Task SubmitNewPassword()
+        {
+            if(!string.IsNullOrWhiteSpace(ChangePassword.CurrentPassword) && string.IsNullOrWhiteSpace(ChangePassword.NewPassword)
+               && ConfirmPassword.Equals(ChangePassword.NewPassword))
+            {
+                using var loadingview = new Components.LoadingView();
+                using var httpClient = new HttpClient(); 
+                httpClient.DefaultRequestHeaders.Authorization = AppStatic.GetAuthenticationHeader();
+                ChangePassword.UserName = AppStatic.User?.UserName;
+                var response = await httpClient.PostAsJsonAsync($"{BLL.Settings.Connections.GetServerAddress()}/api/account/changepassword", ChangePassword);
+                if (response.IsSuccessStatusCode)
+                {
+                    await DisplayAlert("Opration Successfulley", "The password has been changed successfully", "Ok").ConfigureAwait(false);
+                    CloseCurrentPage();
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    await DisplayAlert("An error occurred", error, "Ok").ConfigureAwait(false);
+                }
+            }
+            else
+            {
+                await DisplayAlert("Required Field", "Please, don't leave empty fields and must new password field matching with confirm password field", "Ok").ConfigureAwait(false);
+            }
         }
         private async Task SingIn()
         {
