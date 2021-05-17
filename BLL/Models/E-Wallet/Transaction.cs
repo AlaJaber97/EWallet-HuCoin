1bf88bb0bdaf797596c19c2b503187dd761324e8
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-05-17 19:08:38 +0300

    -add database to save beneficiary -create method transaction

--- BLL/Models/E-Wallet/Transaction.cs
+++ BLL/Models/E-Wallet/Transaction.cs
@@ -4,14 +4,16 @@ using System.Text;
 
 namespace BLL.Models
 {
-    public class TransactionClient
+    public class Transaction
     {
-        public string PrivateKey { get; set; }
-        public string Sender { get; set; }
+        public string RecipientPhoneNumber { get; set; }
         public string Recipient { get; set; }
+        public string Sender { get; set; }
+        public string Signature { get; set; }
+
         public decimal Amount { get; set; }
         public decimal Fees { get; set; }
-        public string Signature { get; set; }
+
         public override string ToString() =>
             $"{Amount:0.00000000}{Recipient}{Sender}";
     }
--- HuCoin/HuCoin/ViewModels/AddBeneficiaryPageViewModel.cs
+++ HuCoin/HuCoin/ViewModels/AddBeneficiaryPageViewModel.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 
 namespace HuCoin.ViewModels
 {
     public class AddBeneficiaryPageViewModel : ViewModels.BaseViewModel
     {
+        public ICommand SaveBeneficiaryCommand { get; set; }
+        public Models.Beneficiary Beneficiary { get; set; }
         public AddBeneficiaryPageViewModel()
         {
-
+            Beneficiary = new Models.Beneficiary();
+            SaveBeneficiaryCommand = new Command(()=> SaveBeneficiary().ConfigureAwait(false));
+        }
+        private async Task SaveBeneficiary()
+        {
+            try
+            {
+                using var loadingview = new Components.LoadingView();
+                using var db = new Data.DbCon();
+                db.Beneficiaries.Add(Beneficiary);
+                if (await db.SaveChangesAsync() > 0)
+                {   
+                    CloseCurrentModal();
+                    MessagingCenter.Send(this, "AddNewBeneficiary");
+                }
+                else
+                {
+                    await DisplayAlert("Add new beneficiary", "can not add new beneficiary", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Add new beneficiary", ex.ToString(), "Ok");
+            }
         }
     }
 }
